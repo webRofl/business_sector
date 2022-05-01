1bc33a22498a8e3b833ab0276697f636285e34c2
webRofl <kakaxa1928@mail.ru>
2022-05-01 21:29:52 +0400

    create search field

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import FindUser from './components/FindUser/FindUser';
 import MainTable from './components/MainTable/MainTable';
 import TableControls from './components/TableControls/TableControls';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from './App.module.css';
 
-const App = () => {
+type AppProps = {};
+
+const App: React.FC<AppProps> = () => {
   return (
-    <div>
+    <main className={classes.mainWrapper}>
       <FindUser />
       <MainTable />
       <TableControls />
-    </div>
+    </main>
   );
 };
 
--- src/components/FindUser/FindUser.tsx
+++ src/components/FindUser/FindUser.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import classes from './FindUser.module.css';
+import findIcon from '../../assets/images/find-icon.svg';
 
 type FindUserProps = {};
 
 const FindUser: React.FC<FindUserProps> = () => {
-  return <div>input</div>;
+  return (
+    <div className={classes.findUser__inputBlock}>
+      <input
+        type="text"
+        className={classes.findUser__input}
+        placeholder="Поиск"
+      />
+      <img
+        src={findIcon}
+        alt="find icon"
+        className={classes.findUser__findIcon}
+      />
+    </div>
+  );
 };
 
 export default FindUser;
