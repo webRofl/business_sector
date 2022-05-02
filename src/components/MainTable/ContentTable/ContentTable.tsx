55cea7f36806a806f8c33b91a30a6d6afb854862
webRofl <kakaxa1928@mail.ru>
2022-05-02 15:22:57 +0400

    create main table

--- src/components/MainTable/ContentTable/ContentTable.tsx
+++ src/components/MainTable/ContentTable/ContentTable.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import classes from './ContentTable.module.css';
 
 const ContentTable = () => {
-  return <p>content</p>;
+  return (
+    <table>
+      <tbody>
+        <tr className={classes.mainTable__tr}>
+          <td>1</td>
+          <td>
+            sunt aut facere repellat provident occaecati excepturi optio
+            reprehenderit
+          </td>
+          <td>
+            quia et suscipit\nsuscipit recusandae consequuntur expedita et
+            cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est
+            autem sunt rem eveniet architecto
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default ContentTable;
--- src/components/MainTable/HeaderTable/HeaderTable.tsx
+++ src/components/MainTable/HeaderTable/HeaderTable.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import classes from './HeaderTable.module.css';
+import arrowUp from '../../../assets/images/arrow-up-icon.svg';
 
 type HeaderTableProps = {};
 
 const HeaderTable: React.FC<HeaderTableProps> = () => {
   return (
-    <div className={classes.mainTable__headerBlock}>
-      <div className={classes.mainTable__headerArrowBlock}>
-        <h3>ID</h3>
-        <div className={classes.mainTable__headerArrow}></div>
-        <div className={classes.mainTable__headerArrow}></div>
+    <div className={classes.headerTable__wrapper}>
+      <div
+        className={`${classes.headerTable__infoWrapper} ${classes.headerTable__idBlock}`}
+      >
+        <span>ID</span>
+        <img
+          src={arrowUp}
+          alt="arrow up icon"
+          className={classes.headerTable__headerArrow}
+        />
       </div>
-      <div className={classes.mainTable__headerArrowBlock}>
-        <h3>Заголовок</h3>
-        <div className={classes.mainTable__headerArrow}></div>
-        <div className={classes.mainTable__headerArrow}></div>
+      <div
+        className={`${classes.headerTable__infoWrapper} ${classes.headerTable__titleBlock}`}
+      >
+        <span>Заголовок</span>
+        <img
+          src={arrowUp}
+          alt="arrow up icon"
+          className={classes.headerTable__headerArrow}
+        />
       </div>
-      <div className={classes.mainTable__headerArrowBlock}>
-        <h3>Описание</h3>
-        <div className={classes.mainTable__headerArrow}></div>
-        <div className={classes.mainTable__headerArrow}></div>
+      <div
+        className={`${classes.headerTable__infoWrapper} ${classes.headerTable__descriptionBlock}`}
+      >
+        <span>Описание</span>
+        <img
+          src={arrowUp}
+          alt="arrow up icon"
+          className={classes.headerTable__headerArrow}
+        />
       </div>
     </div>
   );
